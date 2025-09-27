7ec59c7971b22beaf5a295059d8c74356957a51e
Adam Markenson <adam@presto55.com>
2025-09-27 01:32:01 -0400

    correlations documented

--- correlation.py
+++ correlation.py
@@ -30,15 +30,36 @@ for genre in genres:
     
     plt.suptitle(f'Scatterplot Matrix for {genre} Genre', y=1.02)
     ##plt.show()
-    plt.savefig(f'scatterplot_matrix_{genre}.png')
+    plt.savefig(f'./scatterplots/scatterplot_matrix_{genre}.png')
 
     ## Create and save a table with the correlation coefficients
     plt.figure(figsize=(10, 8))
     sns.heatmap(corr, annot=True, fmt=".2f", cmap='coolwarm', square=True, cbar_kws={"shrink": .8})
     plt.title(f'Correlation Coefficients for {genre} Genre')
-    plt.savefig(f'correlation_table_{genre}.png')
-    plt.close()
+    plt.savefig(f'./correlationmatrices/correlation_table_{genre}.png')
+    plt.close('all')
+
 
     print(f'Plots and correlation table for {genre} genre saved.')
 
+    # Isolating lower triangle of the correlation matrix
+    mask = np.triu(np.ones_like(corr, dtype=bool))
+
+    # Finding pairs with high correlation
+    high_corr = corr.where(~mask).stack().reset_index()
+    high_corr = high_corr[high_corr[0].abs() >= 0.5]
+    high_corr.columns = ['Feature 1', 'Feature 2', 'Correlation']
+
+    # Saving high correlation pairs to a CSV file
+    high_corr.to_csv(f'./highcorrelations/high_correlation_{genre}.csv', index=False)
+
+    # Finding pairs with medium correlation
+    medium_corr = corr.where(~mask).stack().reset_index()
+    medium_corr = medium_corr[(medium_corr[0].abs() < 0.5) & (medium_corr[0].abs() >= 0.3)]
+    medium_corr.columns = ['Feature 1', 'Feature 2', 'Correlation']
+
+    # Saving medium correlation pairs to a CSV file
+    medium_corr.to_csv(f'./mediumcorrelations/medium_correlation_{genre}.csv', index=False)
+    print(f'High and medium correlation pairs for {genre} genre saved.\n')
+    
     ##break  # Remove this break to plot for all genres
